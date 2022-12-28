33fd435c8f62330c9c97b87309b2583608cbe3f5
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-12-29 00:15:29 +0300

    anotha one 3.0

---
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -0,0 +1,7 @@
+SELECT * FROM mark WHERE mark>6 ORDER BY mark DESC;
+
+SELECT * FROM payment WHERE payment<300 ORDER BY payment ASC;
+
+SELECT * FROM paymenttype ORDER BY name ASC;
+
+SELECT * FROM student ORDER BY name DESC;
