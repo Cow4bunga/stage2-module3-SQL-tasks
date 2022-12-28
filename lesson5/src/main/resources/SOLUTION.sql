e21bad8de38066c37610671f5d15ab1082368c42
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-12-29 00:06:08 +0300

    anotha one 3.0

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,13 +1,13 @@
 SELECT * FROM payment WHERE amount>=500;
 
-SELECT * FROM student WHERE birthday<'2000-10-10';
+SELECT * FROM student WHERE birthday<='2000-10-10';
 
-SELECT * FROM student WHERE birthday>'2000-10-10' AND groupnumber=10;
+SELECT * FROM student WHERE birthday>='2000-10-10' AND groupnumber=10;
 
-SELECT * FROM student WHERE (name='Mike') OR (groupnumber IN (4,5,6));
+SELECT * FROM student WHERE (name='Mike%') OR (groupnumber IN (4,5,6));
 
-SELECT * FROM payment WHERE DATE(payment_date)>'2000-02-10';
+SELECT * FROM payment WHERE DATE(payment_date)>='2000-02-10';
 
 SELECT * FROM student WHERE name LIKE 'A%';
 
-SELECT * FROM student WHERE ((name='Roxi') and (groupnumber IN (4))) OR ((name='Tallie') and (groupnumber IN (9)));
+SELECT * FROM student WHERE ((name='Roxi%') and (groupnumber IN (4))) OR ((name='Tallie%') and (groupnumber IN (9)));
